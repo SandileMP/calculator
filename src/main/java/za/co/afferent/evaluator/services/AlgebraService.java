37333fe81ad62688e761dd3257ba645d8b2bd99a
Sandile Moses <sandilephensulo@gmail.com>
2021-12-03 22:59:53 +0200

    Removed external library

--- src/main/java/za/co/afferent/evaluator/services/AlgebraService.java
+++ src/main/java/za/co/afferent/evaluator/services/AlgebraService.java
@@ -1,7 +1,6 @@
 package za.co.afferent.evaluator.services;
 
 
-import com.fathzer.soft.javaluator.DoubleEvaluator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
